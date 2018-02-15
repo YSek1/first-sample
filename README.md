6bd0771b5ef9f93280847a468512e9c7101a7dcb
unknown <yutasek163@gmail.com>
2018-02-15 16:03:49 +0900

    DB定義の追加

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# first-sample
+## first-sample
+
+DB値の一覧表示機を提供。<br>
+DBのテーブルは以下の通り。<p>
+
+|カラム名|型|
+|:-------|:-------|
+|ID|char(8)|
+|NAME|varchar(160)|
+|AGE|Intgeger|
--- src/main/java/sample/controller/SampleController.java
+++ src/main/java/sample/controller/SampleController.java
@@ -1,13 +1,14 @@
 package sample.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class SampleController {
 
 	@RequestMapping(value = "/")
 	public String hello() {
-		return "hello world";
+		return "hello";
 	}
 }
